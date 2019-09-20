ffc3ca309a8130449bcd466804c62495d50b544e
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-20 05:26:31 +0000

    added the program

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - Check if a number is positive, negative, or zero
+ * description: assigns random numbers to the variable, n
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero/n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
+
