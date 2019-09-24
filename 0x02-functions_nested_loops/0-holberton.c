2e2641540c7c84ed8979dc2197a9b65ebee143d2
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-24 16:27:42 +0000

    added files

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* main - prints Holberton followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+char s[] = "Holberton\n";
+while (s[i])
+{
+_putchar (s[i]);
+itt;
+}
+return (0);
+}
