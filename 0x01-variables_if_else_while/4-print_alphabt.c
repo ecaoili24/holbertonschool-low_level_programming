a224483e63e2bb68a6d3859845b3be9a6159f2a7
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-24 05:08:20 +0000

    changed the file name

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - print a-z, without q and e
+* description - uses while statement
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+c = 'a';
+while (c < == 'z')
+{
+if (!(c == 'q' || c == 'e'))
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
