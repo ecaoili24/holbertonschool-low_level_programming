48ba222923c01036c6a57f63803bba876d2e0e5f
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-25 02:59:25 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* _abs - computes the absolute value of an integer
+* @a: any integer
+* Return: a for absolute integer
+*/
+int _abs(int a)
+{
+if (a < 0)
+return (a = -a);
+else
+return (a);
+}
