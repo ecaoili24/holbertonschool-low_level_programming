5502d693b12b13086771f053aa24cdf1ebf35e18
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-25 06:57:51 +0000

    added files

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -9,6 +9,6 @@ int largest_number(int a, int b, int c);
 
 int convert_day(int month, int day);
 
-void print_remaining_days(int month, inth day, int year);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* HOLBERTON_H */
