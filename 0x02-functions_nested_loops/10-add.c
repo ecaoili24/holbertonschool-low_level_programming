5963a13cd94d78edf176eff13b09702453fbe0f5
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-25 04:17:24 +0000

    a plus b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* add - add the values of a and b
+* @a: an integer
+* @b: another integer
+* Return: the value of a + b
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
