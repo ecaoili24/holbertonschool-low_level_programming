a0862cc072b666a27e9e01a484d1efebddec85a8
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-25 03:54:23 +0000

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* jack_bauer - print every minute of the day
+*/
+void jack_bauer(void)
+{
+int hour;
+int minute;
+for (hour = 0; hour < 24; hour++)
+{
+for (hour = 0; minute < 60; minute++)
+{
+_putchar(hour / 10 + '0');
+_putchar(hour % 10 + '0');
+_putchar(':');
+_putchar(minute / 10 + '0');
+_putchar(minute % 10 + '0');
+_putchar('\n');
+}
+}
+}
