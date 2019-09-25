9617de5cda980e5bc4c94f15af937922312c72c6
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-25 06:45:28 +0000

    changed the number of lines

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,6 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 /*
 * i = 0;
 *
@@ -19,7 +18,6 @@ printf("Infinite loop incoming :(\n");
 * putchar(i);
 * }
 */
-
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
