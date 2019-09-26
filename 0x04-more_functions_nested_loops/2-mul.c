a8290dfcab057dc929b6d41437af2f9f33cb744a
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-26 19:58:01 +0000

    changed to the multiply sign

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiply two integers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Return: the product for a + b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
