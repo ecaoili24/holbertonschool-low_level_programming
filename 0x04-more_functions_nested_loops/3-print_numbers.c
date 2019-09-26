5f9a290b78a8d11accb0cdc3de5b9c82680ee113
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-26 21:23:39 +0000

    changed the my variable to char

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,19 @@
 
 /**
  * print_numbers - Print numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = '0'; i <= '9'; i++)
-		_putchar(i);
+	c = '0';
 
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
 	_putchar('\n');
 }
