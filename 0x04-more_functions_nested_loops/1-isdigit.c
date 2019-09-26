4e082aa72fc6ae6687b2b02276a3801dd24f3d3e
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-26 17:35:29 +0000

    check for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - check for a digit from 0-9
+ * @c: the character to be checked
+ * Return: 1 if c is a digit, 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+	    return (1);
+	    else
+		    return (0);
+}
