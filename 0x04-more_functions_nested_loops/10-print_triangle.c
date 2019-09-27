5198674919db56faea515ebba0f5740f72133e6a
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-27 07:23:05 +0000

    two triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, t, spaces, hashes;
+
+	for (i = 1; i <= size; i++)
+	{
+	  spaces = size - i;
+	  hashes = size - spaces;
+
+		for (t = 1; t <= spaces; t++)
+		_putchar(' ');
+		for (t = 1; t <= hashes; t++)
+		_putchar('#');
+
+		_putchar('\n');
+	}
+	if (size <= 0)
+	_putchar('\n');
+}
