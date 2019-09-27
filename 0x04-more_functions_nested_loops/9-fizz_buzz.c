a562a0d91270660dfc10f393c6e802a919910c81
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-09-27 06:05:16 +0000

    Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 1 to 100 "Fizz-Buzz test"
+ * Description: Print numbers from 1 to 100, replacing multiples of 3 with Fizz
+ * and for multiples of 5, print Buzz. Multiples of both 3 and five print Fizz
+ * Buzz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 ==0)
+			printf ("Buzz");
+		else
+			printf("%d", i);
+
+		if (i < 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
