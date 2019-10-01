08f3d38062b341ceb634c810008c4137dc13a354
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-01 21:54:44 +0000

    reverse and swap

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: the string to reverse
+ *
+ */
+
+void rev_string(char *s)
+{
+	int length;
+	char *t = s;
+	int i;
+
+	for (length = 0; *t != '\0'; t++)
+		length++;
+
+	t--;
+	length--;
+
+	for (i = 0; i <= length/2; i++)
+	{
+char tmp = *t;
+	*t = *s;
+	*s = tmp;
+	t--;
+	s++;
+	}
+}
