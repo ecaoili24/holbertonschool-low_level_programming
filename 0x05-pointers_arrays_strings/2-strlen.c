b07f5192a9f6426d38917fa9be3bf76b700933f2
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-01 16:53:05 +0000

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - length of a string
+ * @s: the string to check
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+int length;
+length = 0;
+
+	for (length = 0; *s != '\0'; s++)
+	{
+	length++;
+	}
+return (length);
+}
