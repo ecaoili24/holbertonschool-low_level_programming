76261406bf5bbea3eba58cfcbda37a302e87ea4d
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-01 22:45:32 +0000

    prints out every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include 'holberton.h'
+
+/**
+ * puts2- prints out every other character of a string
+ * @str: the string to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0', i++)
+	{
+		if (i % 2 == 0)
+			_putchar(*str);
+		str++;
+	}
+	_putchar('/n');
+}
