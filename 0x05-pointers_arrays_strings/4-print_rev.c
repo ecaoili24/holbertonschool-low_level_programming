d88ceed6a5168212feb2045ab7561113df7508f6
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-01 19:57:56 +0000

    in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: the string to print
+ */
+void print_rev(char *s)
+{
+	int length;
+
+for (length = 0; *s != '\0'; s++)
+{
+length += 1;
+}
+
+length -= -1;
+
+for (; length >= 0; length--, s--)
+	_putchar(*s);
+
+	_putchar('\n');
+}
