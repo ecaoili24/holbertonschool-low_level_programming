cc10a786e424a654065620a8b24babb1854b199b
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-04 02:52:30 +0000

    combines two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @src: the first string, Hello
+ * @dest: the second string, World
+ * Return: dest string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = _strlen(dest);
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[len + i] = src[i];
+			dest[len + i] = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * _strlen - length of a string
+ * @s: the string to check
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+length = 0;
+
+	for (length = 0; *s != '\0'; s++)
+	{
+		length++;
+	}
+	return (length);
+}
