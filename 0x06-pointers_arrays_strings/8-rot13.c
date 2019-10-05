1e10a1040409aa6261a431638271614227c6b652
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-05 07:16:58 +0000

    rotate

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encodes a string by simple letter substitution.
+ * It replaces a letter with the 13th letter after it, in the alphabet.
+ * @s: the string to encode
+ *
+ * Return: the encoded message
+ */
+char *rot13(char *s)
+{
+	int i, x;
+	char input[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+			'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+			'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+			's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+	char rotate[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+			 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+			 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
+			 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',
+			 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (x = 0; x < 52; x++)
+	{
+		if (s[i] == input[i])
+		{
+			s[i] = rotate[x];
+			x = 52;
+		}
+	}
+	}
+	return (s);
+}
