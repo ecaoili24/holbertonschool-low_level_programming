c20832b03259d55628791a87525feb3af5a29982
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-07 20:01:24 +0000

    character c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @c: the character to look for in a string
+ * @s: the string
+ *
+ * Return: a pointer to the first occurance of the character c or NULL if the
+ * character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (&s[i]);
+
+	return ('\0');
+}
