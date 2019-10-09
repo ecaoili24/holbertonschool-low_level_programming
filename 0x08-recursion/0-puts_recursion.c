091976b02acdadfe4bbffbcd77b4931fa626991a
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-09 21:32:57 +0000

    added a basecase

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[0]);
-		_puts_recursion(s);
-		s++;
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+
+	if (*s != '\0')
+	{	_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
