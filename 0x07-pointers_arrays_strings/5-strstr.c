7d3daba75a2ce29a8c3dab79f717918051572a04
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-09 03:33:45 +0000

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strstr - finds the first occurence of the substring needle in the string
+ * @haystack: the string to search
+ * @needle: the substring to search for
+ *
+ * Return: a pointer to the beginning of the located substring or NULL if the
+ * substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+	{
+		if (haystack[i + j] != needle[j])
+			break;
+
+		if (needle[j + 1] == '\0')
+			return (haystack + i);
+	}
+	}
+	return (0);
+}
