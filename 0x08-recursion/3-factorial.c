be2958cbf86b417d8a019f704b7af948be18c423
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-10 05:50:27 +0000

    changed betty style

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,7 +4,7 @@
  * factorial - returns the factorial of a given number
  * @n: a number
  *
- * Return: if n is less than 0, the function returns -1 and if it's equals 0, it
+ * Return: if n is less than 0, the function returns -1 and if it's equals 0,
  * it returns 1
  */
 int factorial(int n)
