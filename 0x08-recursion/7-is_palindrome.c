78cf9f56b047484a26d4688ef2531b525af1e6cb
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-10 06:46:51 +0000

    changed to two functions

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,51 +1,42 @@
 #include "holberton.h"
 
 /**
- * is_palindrome - returns a palindrome
- * @s: the string to check
- *
- * Return: returns 1 is a string is a palindrome and 0 if not
- */
-int is_palindrome(char *s)
-{
-	int len;
-	len = *s;
-
-	if (*s != '\0')
-		return (1);
-
-	return (palindrome_checker(s, len, 0, len - 1));
-}
-/**
  * palindrome_checker - a helper function to recursively check if s is a
  * palindrome
  * @s: the string to check
- * @len: the length of s
  * @letter1: the first letter to check in s
  * @letter2: the next letter to check in s
  *
  * Return: 1 if the string is a palindrome and 0, if not
  */
-int palindrome_checker(char *s, int len, int letter1, int letter2)
+int palindrome_checker(char *s, int letter1, int letter2)
 {
-	if (letter1 >= letter2)
+	if (letter1 == letter2)
 		return (1);
 
 	if (s[letter1] != s[letter2])
 		return (0);
-	else
-	return (palindrome_checker(s, len, letter1 + 1, letter2 - 1));
+
+	if (letter1 < letter2 + 1)
+		return (palindrome_checker(s, letter1 + 1, letter2 - 1));
+	return (1);
 }
 /**
- * _strlen_recursion - gets the length of a string
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
  * @s: the string
  *
- * Return: the length of the string
+ * Return: an integer whether it's 0 or 1
  */
-int _strlen_recursion(char *s)
+int is_palindrome(char *s)
 {
-	if (*s == '\0')
-		return (0);
+	int i;
+	int counter = 0;
 
-	return (_strlen_recursion(s + 1) + 1);
+	for (i = 0; s[i] != '\0'; i++)
+		counter += 1;
+
+	if (counter == 0)
+		return (1);
+
+	return (palindrome_checker(s, 0, counter - 1));
 }
