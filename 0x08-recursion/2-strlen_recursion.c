756ee8420be1a48a151985d5be2f4135c2113245
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-10 02:35:26 +0000

    length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - gets the length of a string
+ * @s: the string
+ *
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (_strlen_recursion(s + 1) + 1);
+}
