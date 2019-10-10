818fde94a01904180f5c8811e898b3e880e4fc3d
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-10 01:59:27 +0000

    reverse a string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string to reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
