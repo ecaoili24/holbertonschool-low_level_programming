9c25ee669bd26ae99000a38f63afdee53868684a
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-13 15:28:11 +0000

    add files

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[])
+
+#endif /*HOLBERTON_H*/
