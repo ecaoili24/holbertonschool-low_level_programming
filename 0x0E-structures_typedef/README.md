46cf968d6ca3e93677961a61870818ed03021ea0
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-22 00:13:12 +0000

    add files

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+The following project will cover:
+What are structures, when, why and how to use them
+How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a type with the following: name of a dog, dog's age, and dog
+ * owner name. A struct containing a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner's name
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
