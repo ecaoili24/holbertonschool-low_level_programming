fbca18e75239578141aa39ac879e32436b831c34
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-22 01:54:53 +0000

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create a new dog fuction
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog owner's name
+ *
+ * Return: A pointer to a new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	int name_len, owner_len, i;
+
+	for (name_len = 0; name[name_len] != '\0'; name_len++)
+		;
+
+	for (owner_len = 0; owner[owner_len] != '\0'; name_len++)
+		;
+
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->name = malloc(sizeof(char) * (name_len + 1));
+	{
+		free(new);
+	}
+
+	new->owner = malloc(sizeof(char) * (owner_len + 1));
+	{
+		free(new);
+		free(new->name);
+	}
+
+	for (i = 0; i <= name_len; i++)
+		new->name[i] = name[i];
+
+	new->age = age;
+
+	for (i = 0; i <= owner_len; i++)
+		new->owner[i] = owner[i];
+
+	return (new);
+}
