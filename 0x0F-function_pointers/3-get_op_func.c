4e95d33006605db600c127488b2c404ed13f693b
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-23 22:33:13 +0000

    fixed a betty issue

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,18 +7,18 @@
  * the user
  * @s: the operator passes as argument to the program
  *
- * Return: a pointer to the function that corresponds to the operator given as a
- * parameter
+ * Return: a pointer to the function that corresponds to the operator
+ * given as a parameter
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 
 	};
 	int i;
@@ -28,10 +28,10 @@ int (*get_op_func(char *s))(int, int)
 	while (ops[i].op)
 	{
 		if (ops[i].op[0] == s[0])
-		    return (ops[i].f);
-		    i++;
+			return (ops[i].f);
+		i++;
 	}
 
 	printf("Error\n");
-	exit (99);
+	exit(99);
 }
