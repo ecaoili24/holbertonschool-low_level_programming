3c4208a7a6ab3088a761e8f7d86a5884609e9f88
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-24 06:14:13 +0000

    added null case with error and exit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,14 +19,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
-	if (argv[2][0] == '/' && argv[2][0] == '%')
+	if ((get_op_func)(argv[2]) == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-
-	if (atoi(argv[3]) == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
