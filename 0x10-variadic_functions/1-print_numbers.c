ba18072f4e319a214637a7aab4702333f2cce84b
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-25 22:22:37 +0000

    added null basecase

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,6 +12,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list v;
 
+	if (separator == NULL)
+		return;
+
 	va_start(v, n);
 	for (i = 0; i < n; i++)
 	{
