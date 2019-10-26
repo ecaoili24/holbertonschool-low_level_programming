458fa43ce5b9f115fad2aea0fd6026ecf701ec17
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-26 19:55:31 +0000

    removed a typo in the Error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,10 +16,10 @@ int main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
-		printf("Error1\n");
+		printf("Error\n");
 		exit(98);
 	}
-	if ((get_op_func)(argv[2]) == NULL || argv[2][1] != '\0')
+	if ((get_op_func)(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
