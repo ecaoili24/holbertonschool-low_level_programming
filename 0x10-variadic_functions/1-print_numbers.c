bc9628ca5ec1b0ca88956c3a25f98fabe6a2ea4f
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-26 23:01:40 +0000

    added the n-1 so to print the separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,13 +14,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(v, n);
 
-	if (separator == NULL)
-		return;
-
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(v, unsigned int));
-		if (i < n - 1)
+		if (i < n - 1 && i != (n - 1))
 			printf("%s", separator);
 	}
 
