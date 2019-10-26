94745446f38f9cecaf2a863f0fd305e34d360636
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-26 22:35:09 +0000

    removed separator from if statement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,8 +19,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(v, int));
-		if (separator && i < (n - 1))
+		printf("%d", va_arg(v, unsigned int));
+		if (i < n - 1)
 			printf("%s", separator);
 	}
 
