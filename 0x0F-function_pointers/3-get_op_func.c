30c14db56f2e083991d381e1672c25c9588b9f0e
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-26 19:53:29 +0000

    added null byte to if statement

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -27,12 +27,10 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op)
 	{
-		if (ops[i].op[0] == s[0])
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
 
-	printf("Error\n");
-	exit(99);
 	return (NULL);
 }
