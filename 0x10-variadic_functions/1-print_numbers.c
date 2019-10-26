81a8c3bc41eb6985058391c92d368d523f797774
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-26 23:04:04 +0000

    added separator to the if statement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(v, unsigned int));
-		if (i < n - 1 && i != (n - 1))
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 
