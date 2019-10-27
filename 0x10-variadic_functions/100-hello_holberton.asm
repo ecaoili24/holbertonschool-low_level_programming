ff8411b6c31b9d3c155d84d3fc7e4618343adeed
ecaoili24 <erika.caoili@gmail.comgit config --global user.email erika.caoili@gmail.com>
2019-10-27 18:23:33 +0000

    changed the comments

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,14 @@
 		global    main
 
 	        section   .text
-main:	    	mov       rax, 1 ; system call for write
-	        mov       rdi, 1    ; file handle 1 is stdout
-	        mov       rsi, holberton ; address of string to output
-	        mov       rdx, 17	 ; number of bytes
+main:	    	mov       rax, 1	 ; system call for write
+	        mov       rdi, 1    ; the file handle 1 is stdout
+	        mov       rsi, holberton ;the address of string to output
+	        mov       rdx, 17	 ;the number of bytes in the string
 	        syscall		 ; invoke operating system to do the write
 	        mov       rax, 60 ; system call for exit
-	        xor       rdi, rdi	    ; exit code 0
+	        xor       rdi, rdi	    ; the exit code 0
 	        syscall		    ; invoke operating system to exit
 
 	        section   .data
-holberton:	db        "Hello, Holberton", 10 ; note the newline at the end
+holberton:	db        "Hello, Holberton", 10 ; notice the newline at the end
