154c4ab5d27a238246246c664f0d44cf7b19f971
ecaoili24 <erika.caoili@gmail.com>
2019-10-31 21:59:56 +0000

    deleted an extra space

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str)
-			printf("[%d]  %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		else
 			printf("[0] (nil)\n");
