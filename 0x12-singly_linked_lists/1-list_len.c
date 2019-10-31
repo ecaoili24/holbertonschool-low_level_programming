1ba204d23286ad586217689e1480c4ad1b479571
ecaoili24 <erika.caoili@gmail.com>
2019-10-31 22:09:54 +0000

    initial commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - write a function that returns the number of elements
+ * in a linked list
+ * @h: the list
+ *
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h-> next;
+		i++;
+	}
+	return (i);
+}
