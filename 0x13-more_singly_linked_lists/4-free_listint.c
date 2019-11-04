d2b85d3faa86f89ce991ee3719e482954d29634e
ecaoili24 <erika.caoili@gmail.com>
2019-11-04 20:26:45 +0000

    initial commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - a function that frees a list
+ * @head: the pointer to the list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
