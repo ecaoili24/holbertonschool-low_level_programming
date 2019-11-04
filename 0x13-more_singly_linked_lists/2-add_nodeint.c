3c241a7e4c2a797b8b6f7d53cfadba58be157c2e
ecaoili24 <erika.caoili@gmail.com>
2019-11-04 18:56:46 +0000

    changed variable name

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,18 +12,18 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *node;
+	listint_t *start;
 
 	if (head == NULL)
 		return (NULL);
 
-	node = malloc(sizeof(listint_t));
-	if (node == NULL)
+	start = malloc(sizeof(listint_t));
+	if (start == NULL)
 		return (NULL);
 
-	node->n = n;
-	node->next = *head;
+	start->n = n;
+	start->next = *head;
 
-	*head = node;
-	return (node);
+	*head = start;
+	return (start);
 }
