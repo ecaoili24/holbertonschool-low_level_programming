534f9cb6a7c180ddecd7a6978c9a0c423e1bc4e2
ecaoili24 <erika.caoili@gmail.com>
2019-11-05 19:02:36 +0000

    the while condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *start;
 	listint_t *position = *head;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (head == NULL)
 		return (NULL);
