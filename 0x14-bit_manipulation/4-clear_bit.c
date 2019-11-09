59aae69816cd0e547d2434c0df69f550850b34f5
ecaoili24 <erika.caoili@gmail.com>
2019-11-09 07:39:47 +0000

    initial commit of clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * clear_bit - write a function that sets the value of a bit to 0
+ * at a given index
+ * @index: the index of the bit you want to set, starting from 0
+ * @n: the number to convert
+ *
+ * Return: 1 for success, -1 if an error occurs
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int j = 0;
+	unsigned long int flag = 1;
+
+	if (!n)
+		return (-1);
+
+	if (index > 0 && *n == 0)
+		return (1);
+
+	while (flag > 0)
+	{
+		if (j == index)
+		{
+			*n = (flag ^ *n);
+			return (1);
+		}
+
+		j += 1;
+		flag = flag << 1;
+	}
+	return (-1);
+}
