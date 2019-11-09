30a86b1c20f538c399fb67a109d234fe4e714b1d
ecaoili24 <erika.caoili@gmail.com>
2019-11-09 08:40:22 +0000

    corrent endianness function

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -11,11 +11,5 @@ int get_endianness(void)
 {
 	unsigned int n = 1;
 	char *c = (char *) &n;
-
-	if (*c)
-		printf("Little endian");
-	else
-		printf("Big endian");
-	getchar();
-	return (0);
+	return (int)*c;
 }
