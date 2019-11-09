10e55fcf27a6a2ffea6ab26aa3e2dbe5a27e338a
ecaoili24 <erika.caoili@gmail.com>
2019-11-09 09:04:30 +0000

    fixed betty issues

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,13 +3,13 @@
 
 /**
  * get_endianness - a function that checks the endianness
- * @n: the number
  *
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-	unsigned int n = 1;
-	char *c = (char *) &n;
-	return (int)*c;
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return ((int) *c);
 }
