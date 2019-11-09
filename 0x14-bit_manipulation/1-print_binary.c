15771898fff214592076314ca6c0259ee15c766c
ecaoili24 <erika.caoili@gmail.com>
2019-11-09 07:07:24 +0000

    initial commit of print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * void print_binary - a function that prints the binary representation
+ * of a number
+ * @n: the number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
