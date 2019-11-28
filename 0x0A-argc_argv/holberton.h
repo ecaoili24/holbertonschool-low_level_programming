d260268793ea50960923e4f74b559c4a2d30f755
ecaoili24 <erika.caoili@gmail.com>
2019-11-28 18:09:11 +0000

    fixed an error and added a statement terminator to prototype

--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -4,6 +4,6 @@
 int _putchar(char c);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]);
 
 #endif /*HOLBERTON_H*/
