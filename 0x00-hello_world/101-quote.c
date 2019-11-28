5b6e1ed1cf90d5f9f93c8fe58e0a30799f8df00c
ecaoili24 <erika.caoili@gmail.com>
2019-11-28 05:11:39 +0000

    changed the return description, removed the standard input and output call, used the correct exit value, and added an escape characters for the quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return 1 if success.
+ *
+ * Return: Always (1) for error
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n", 59);
 	return (1);
 }
