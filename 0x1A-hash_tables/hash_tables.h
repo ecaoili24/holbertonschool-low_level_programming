8fdb7a920d4118377d2c9ef1c69b8e8cb3a9108f
ecaoili24 <erika.caoili@gmail.com>
2020-01-22 06:27:21 +0000

    updated the header file to add a prototype and to delete duplicate prototypes

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,10 +38,9 @@ typedef struct hash_table_s
 /* functions */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 int attach_node_to_ht(hash_table_t *ht, hash_node_t *node);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
