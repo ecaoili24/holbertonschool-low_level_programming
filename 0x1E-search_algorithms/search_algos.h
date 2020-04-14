ac80488391c52ac8cccb6fca677401f88f6d6946
ecaoili24 <erika.caoili@gmail.com>
2020-04-14 19:06:54 +0000

    Fixed prototype names

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,7 +8,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
-void print_array(int *arr, int l, int r);
-int find_binary_s earch(int *arr, int l, int r, int x);
+void print_array(int *array, int left, int right);
+int find_binary_search(int *array, int left, int right, int i);
 
 #endif /* SEARCH_ALGOS_H */
