b94fd7f399a392da5cba2585ac11cede446b8f9d
ecaoili24 <erika.caoili@gmail.com>
2020-04-14 17:27:15 +0000

    Initial commit of the header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#end if /* SEARCH_ALGOS_H */
