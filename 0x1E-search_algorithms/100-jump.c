f77878640b1e62e1a39bed4d1d087d509408cacf
ecaoili24 <erika.caoili@gmail.com>
2020-04-16 20:40:48 +0000

    Added comments to my code

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,30 +11,31 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t x = 0;
-	int step = sqrt(size);
+	size_t x = 0; /* the indx */
+	int step = sqrt(size); /* gets the size of the array */
 
-	if (!array || size == 0)
+	if (!array || size == 0) /* checks if the arr is NULL or the size = 0 */
 		return (-1);
 
-	while (array[x] < value)
+	while (array[x] < value) /* jump search */
 	{
 		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
 		x += step;
-		if (x >= size)
+		if (x >= size)/* exits when the idx is greater or = to size */
 			break;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", x - step, x);
 	x -= step;
 
-	while (array[x] < value)
+	while (array[x] < value) /* linear search */
 	{
 		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
 		x++;
-		if (x == size)
+		if (x == size) /* exits when idx = size */
 			return (-1);
 	}
-	if (array[x] == value)
+
+	if (array[x] == value) /* returns the index */
 	{
 		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
 		return (x);
