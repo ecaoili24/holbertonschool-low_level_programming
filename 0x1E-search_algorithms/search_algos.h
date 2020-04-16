2e36c0f27680e371438f6a8e3bceeb8db19e3aab
ecaoili24 <erika.caoili@gmail.com>
2020-04-16 19:31:31 +0000

    fixed a typo on the prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,12 +3,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
 void print_array(int *array, int left, int right);
 int find_binary_search(int *array, int left, int right, int i);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
